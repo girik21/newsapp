fce98918ee0ff819aaed5311fb4939e4924481e9
Girik21 <girikaustuv@gmail.com>
2021-08-29 00:48:21 +0545

    init

--- news-site/pages/index.js
+++ news-site/pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Layout from '../components/layout'
 import styles from '../styles/index.module.css';
-import Link from 'next/link' 
+
 
 
 
@@ -27,7 +27,7 @@ export default function Home() {
            <h1><u>HELLO!</u></h1>
           
            <h2><b>Welcome to News 24 </b></h2>
-             <h5>Your favourite stop for all the latest trending news going all around the world! Click on the "Feed" option to get started.</h5>
+             <h5>Your favourite stop for all the latest trending news going all around the world! Click on the Feed option to get started.</h5>
             
            <h3>Have a suggestion for us? </h3>
            <h4>Please write us at   <u>newsnoreply@gmail.com</u></h4>
@@ -38,4 +38,5 @@ export default function Home() {
            </div>
      
          
-);}
+);
+}
