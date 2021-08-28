035fe42e0079d423294324426a669c091695100a
Girik21 <girikaustuv@gmail.com>
2021-08-29 00:30:42 +0545

    init

--- news-site/pages/index.js
+++ news-site/pages/index.js
@@ -27,8 +27,8 @@ export default function Home() {
            <h1><u>HELLO!</u></h1>
           
            <h2><b>Welcome to News 24 </b></h2>
-             <h5>Your favourite stop for all the latest trending news going all around the world! Click on the "Latest News" option to get started.</h5>
-            <a Link="/feed/1"> <button type="submit" className="w-half rounded bg-green-500 hover:bg-red-600 text-black py-2 px-4">Latest News</button></a>
+             <h5>Your favourite stop for all the latest trending news going all around the world! Click on the "Feed" option to get started.</h5>
+            
            <h3>Have a suggestion for us? </h3>
            <h4>Please write us at   <u>newsnoreply@gmail.com</u></h4>
 
