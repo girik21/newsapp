76d44ae1f9d4993c35dadd929f77dac49d61f265
Girik21 <girikaustuv@gmail.com>
2021-08-29 15:17:14 +0545

    init

--- news-site/next.config.js
+++ news-site/next.config.js
@@ -1,3 +1,13 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const withPlugins = require('next-compose-plugins');
+const withTM = require('next-transpile-modules')(['@edooking/ui']);
+const withOptimizedImages = require('next-optimized-images');
+const withFonts = require('next-fonts');
+
+const nextJsConfig = {
+  target: 'serverless',
+};
+
+module.exports = withPlugins(
+  [withTM, withOptimizedImages, withFonts],
+  nextJsConfig,
+);
